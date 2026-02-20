03019467eba1274d125f15039f7318a73c3f37e6
turalgasimov <turallgasimov@gmail.com>
2026-02-20 10:30:45 +0000

    simple version

--- main.c
+++ main.c
@@ -1,11 +1,56 @@
 #include "shell.h"
 
 /**
- * main - entry point
- *
- * Return: 0
-*/
+ * main - simple shell
+ * Return: Always 0
+ */
 int main(void)
 {
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		printf("($) ");
+		fflush(stdout);
+
+		read = getline(&line, &len, stdin);
+
+		/* Handle EOF (Ctrl+D) */
+		if (read == -1)
+		{
+			printf("\n");
+			free(line);
+			exit(0);
+		}
+
+		/* Remove newline */
+		line[read - 1] = '\0';
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("Error");
+			free(line);
+			exit(1);
+		}
+
+		if (pid == 0)
+		{
+			execve(line, NULL, environ);
+			perror("./hsh");
+			exit(1);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
+	free(line);
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -3,5 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
 
 #endif
